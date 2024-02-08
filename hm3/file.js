7a6977ae1fdd5c52a29f7fdd9e5d4a90ac2c9b67
Polina <157054604+FikkaP@users.noreply.github.com>
2024-02-08 22:10:47 +0300

    file.js
    rewrote old mistakes

--- hm3/file.js
+++ hm3/file.js
@@ -2,19 +2,20 @@ console.log('polina'+ false)
 console.log('polinka'+ 8)
 console.log(45+ false)
 console.log(45+ true)
+
 console.log('polina'* false)
 console.log('polinka'* 4)
 console.log(32*true)
 console.log(32*false)
+
 console.log('polina'/ true)
 console.log('polinka'/ 3)
 console.log(12/ true)
 console.log(12/ false)
 
-let name = String('Polina');
-console.log(name);
-
-let num = Number(123);
-console.log(num)
-
-console.log(Boolean(6))
+console.log(Number(`123`))
+console.log(Number(true))
+console.log(String(123))
+console.log(String(true))
+console.log(Boolean(1))
+console.log(Boolean(``))
